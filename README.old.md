a2d9753fdfdddaf0dc02c8ae1f8f2a80430df733
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-26 13:50:07 -0500

    3rd time trying to get React and Git to cooperate initial commit

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# front-end
+Good luck and have fun ♥️
+
+Tasks 05/26/2020
+- restarted setting up React for the 3rd time
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>Login / Register from will go here</h1>
+    </div>
+  );
+}
+
+export default App;
