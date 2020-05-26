39750d696bb5e9da3dee706673866db26b7073d8
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-26 14:27:10 -0500

    register form done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
   <h1>Login / Register from will go here</h1>
+  <RegisterForm />
     </div>
   );
 }
