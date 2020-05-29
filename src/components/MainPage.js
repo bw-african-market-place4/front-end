a41861a3692621ff9780cd02373e661648e0056c
Wayne <wabbruscatoorr@gmail.com>
2020-05-28 20:14:14 -0400

    updating mainpage file, addItem file and imported these files to app file

---
+++ src/components/MainPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+import AddItem from "./AddItem";
+
+const MainPage = () => {
+  axios
+    .get("https://api.github.com/users/Callisto1981/followers")
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
+
+  return (
+    <div className="main-page">
+      <h1>Add Item Here</h1>
+      <AddItem />
+    </div>
+  );
+};
+
+export default MainPage;
